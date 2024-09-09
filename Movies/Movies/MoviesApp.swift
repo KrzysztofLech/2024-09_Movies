0fc81b9cb954fc51427cf8ddc700cbe7d09caf55
Krzysztof Lech <krzysztof.lech@24abc.pl>
2024-09-09 07:27:21 +0200

    Xcode project added

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,13 @@
+//  MoviesApp.swift
+//  Created by Krzysztof Lech on 09/09/2024.
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
